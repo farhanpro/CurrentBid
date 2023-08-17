bfaeaece7f06130e587a2e30f13a4a75a93518c3
farhanpro <shaikhfarhan549@gmail.com>
2023-08-17 15:22:46 +0530

    Second page work

--- src/app/bidding-status/bidding-status.component.ts
+++ src/app/bidding-status/bidding-status.component.ts
@@ -36,13 +36,13 @@ export class BiddingStatusComponent {
 
   expandBiddingStatus : boolean = false;
 
-  biddingStatus = {bidName :'Vacation Bid 101',bidPeriod:'01/08/2024 - 01/05/2025',bidStatus :'In-progress'} 
+  biddingStatus = {bidName :'Vacation Bid 101',bidPeriod:'Jan-08-2024 - Jan-05-2025',bidStatus :'In-progress'} 
 
   top10AvailableBlocks = 
   [
-    {blockno : "Block 1",date:'01/02/2024 - 01/08/2024'},{blockno : "Block 2",date:'01/02/2024 - 01/08/2024'},
-    {blockno : "Block 3",date:'01/02/2024 - 01/08/2024'},{blockno : "Block 4",date:'01/02/2024 - 01/08/2024'},
-    {blockno : "Block 5",date:'01/02/2024 - 01/08/2024'},{blockno : "Block 6",date:'01/02/2024 - 01/08/2024'},
+    {blockno : "Block 1",date:'Jan-08-2024 - Jan-14-2024'},{blockno : "Block 2",date:'Feb-01-2024 - Feb-06-2024'},
+    {blockno : "Block 3",date:'Apr-02-2024 - Apr-08-2024'},{blockno : "Block 4",date:'May-02-2024 - May-08-2024'},
+    {blockno : "Block 5",date:'Jul/02/2024 - Jul/08/2024'},{blockno : "Block 6",date:'Jul/02/2024 - Jul/08/2024'},
   ]
 
   top10IndemandBlocks = [
